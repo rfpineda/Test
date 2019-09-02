8ce5dab34a17ddac00d139b577bf1169ffb71e4f
rfpineda <54775017+rfpineda@users.noreply.github.com>
2019-09-01 23:26:50 -0500

    Add files via upload

--- [SQL-Scripts]/1. Tablas.sql
+++ [SQL-Scripts]/1. Tablas.sql
@@ -1,152 +1,193 @@
--- Usuario
-IF OBJECT_ID('dbo.ActualizarEmpleado' , 'P') IS NOT NULL
-	DROP PROCEDURE dbo.ActualizarEmpleado
+USE [ABC]
 GO
-IF OBJECT_ID('dbo.EliminarEmpleado' , 'P') IS NOT NULL
-	DROP PROCEDURE dbo.EliminarEmpleado
+
+/****** Object:  Table [administracion].[Cliente]    Script Date: 01/09/2019 19:03:06 ******/
+SET ANSI_NULLS ON
 GO
-IF OBJECT_ID('dbo.InsertarEmpleado' , 'P') IS NOT NULL
-	DROP PROCEDURE dbo.InsertarEmpleado
+
+SET QUOTED_IDENTIFIER ON
 GO
-IF OBJECT_ID('dbo.ListarEmpleado' , 'P') IS NOT NULL
-	DROP PROCEDURE dbo.ListarEmpleado
-GO
-IF OBJECT_ID('dbo.ObtenerEmpleado' , 'P') IS NOT NULL
-	DROP PROCEDURE dbo.ObtenerEmpleado
-GO
-CREATE PROCEDURE dbo.ActualizarEmpleado
+
+CREATE TABLE [administracion].[Cliente](
+	[IdCliente] [int] IDENTITY(1,1) NOT NULL,
+	[Nit] [int] NOT NULL,
+	[Nombre] [varchar](100) NOT NULL,
+	[Descripcion] [varchar](300) NULL,
+ CONSTRAINT [PK_Cliente] PRIMARY KEY CLUSTERED 
 (
-	@idUsuario VARCHAR(50),
-	@identificacion BIGINT,	
-	@nombre VARCHAR(50),
-	@primerApellido VARCHAR(50),
-	@segundoApellido VARCHAR(50),
-	@correoElectronico VARCHAR(100),
-	@constrasena VARCHAR(2000), 
-	@esActivo BIT	
-)
-AS
-BEGIN
-	UPDATE dbo.Usuario 
-	SET Identificacion = @identificacion,
-		Nombre = @nombre,
-		PrimerApellido = @primerApellido,
-		SegundoApellido = @segundoApellido,
-		CorreoElectronico = @correoElectronico,
-		EsActivo = @esActivo,
-		CambioContrasena = 0,
-		IntentosAutenticacion = 0,
-		MomentoUltimaActualizacion = GETDATE()
-	WHERE IdUsuario = @idUsuario
-END
+	[IdCliente] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 GO
 
-CREATE PROCEDURE dbo.EliminarUsuario
+USE [ABC]
+GO
+
+/****** Object:  Table [administracion].[Empleado]    Script Date: 01/09/2019 19:03:32 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [administracion].[Empleado](
+	[IdUsuario] [varchar](50) NOT NULL,
+	[Identificacion] [int] NOT NULL,
+	[Nombre] [varchar](100) NOT NULL,
+	[PrimerApellido] [varchar](100) NOT NULL,
+	[SegundoApellido] [varchar](100) NULL,
+	[Contrasena] [varchar](300) NOT NULL,
+ CONSTRAINT [PK_Empleado] PRIMARY KEY CLUSTERED 
 (
-	@idUsuario VARCHAR(50)
-)
-AS
-BEGIN
-    DELETE
-	FROM dbo.Empleado
-    WHERE
-		IdUsuario = @idUsuario
-END
+	[IdUsuario] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 GO
 
-CREATE PROCEDURE dbo.InsertarUsuario
-(	
-	@idUsuario VARCHAR(50),
-	@identificacion BIGINT,	
-	@nombre VARCHAR(50),
-	@primerApellido VARCHAR(50),
-	@segundoApellido VARCHAR(50),
-	@correoElectronico VARCHAR(100)
-)
-AS
-BEGIN
-	INSERT INTO dbo.Usuario
-	(
-		IdUsuario,
-		Nombre,
-		PrimerApellido,
-		SegundoApellido,
-		CorreoElectronico,
-		Contrasena,
-		EsActivo,
-		CambioContrasena,
-		IntentosAutenticacion,
-		MomentoCreacion,
-		MomentoUltimaActualizacion
-	)
-	VALUES 
-	(		
-		@idUsuario,
-		@nombre,
-		@primerApellido,
-		@segundoApellido,
-		@correoElectronico,
-		'',
-		1,
-		1,
-		0,
-		GETDATE(),
-		GETDATE()		
-	)
-END
+USE [ABC]
 GO
 
-CREATE PROCEDURE dbo.ListarUsuario
-AS
-BEGIN
-	SELECT
-		*
-	FROM dbo.Usuario
-END
+/****** Object:  Table [administracion].[Perfil]    Script Date: 01/09/2019 19:03:58 ******/
+SET ANSI_NULLS ON
 GO
 
-CREATE PROCEDURE dbo.ObtenerUsuario
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [administracion].[Perfil](
+	[IdPerfil] [int] IDENTITY(1,1) NOT NULL,
+	[Descripcion] [varchar](200) NOT NULL,
+	[Codigo] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_Perfil] PRIMARY KEY CLUSTERED 
 (
-	@idUsuario VARCHAR(50)
-)
-AS
-BEGIN
-    SELECT
-		*
-	FROM dbo.Usuario
-    WHERE
-		IdUsuario = @idUsuario
-END
+	[IdPerfil] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 GO
 
--- LinkerBoxProtocolo
-
-IF OBJECT_ID('dbo.ListarLinkerBoxProtocolo' , 'P') IS NOT NULL
-	DROP PROCEDURE dbo.ListarLinkerBoxProtocolo
-GO
-IF OBJECT_ID('dbo.ObtenerLinkerBoxProtocolo' , 'P') IS NOT NULL
-	DROP PROCEDURE dbo.ObtenerLinkerBoxProtocolo
+USE [ABC]
 GO
 
-CREATE PROCEDURE dbo.ListarLinkerBoxProtocolo
-AS
-BEGIN
-	SELECT
-		*
-	FROM dbo.LinkerBoxProtocolo
-END
+/****** Object:  Table [administracion].[EmpleadoPerfil]    Script Date: 01/09/2019 19:04:24 ******/
+SET ANSI_NULLS ON
 GO
 
-CREATE PROCEDURE dbo.ObtenerLinkerBoxProtocolo
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [administracion].[EmpleadoPerfil](
+	[IdUsuario] [varchar](50) NOT NULL,
+	[IdPerfil] [int] NOT NULL
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [administracion].[EmpleadoPerfil]  WITH CHECK ADD  CONSTRAINT [FK_EmpleadoPerfil_Empleado1] FOREIGN KEY([IdUsuario])
+REFERENCES [administracion].[Empleado] ([IdUsuario])
+GO
+
+ALTER TABLE [administracion].[EmpleadoPerfil] CHECK CONSTRAINT [FK_EmpleadoPerfil_Empleado1]
+GO
+
+ALTER TABLE [administracion].[EmpleadoPerfil]  WITH CHECK ADD  CONSTRAINT [FK_EmpleadoPerfil_Perfil] FOREIGN KEY([IdPerfil])
+REFERENCES [administracion].[Perfil] ([IdPerfil])
+GO
+
+ALTER TABLE [administracion].[EmpleadoPerfil] CHECK CONSTRAINT [FK_EmpleadoPerfil_Perfil]
+GO
+
+
+USE [ABC]
+GO
+
+/****** Object:  Table [facturacion].[Factura]    Script Date: 01/09/2019 19:04:59 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [facturacion].[Factura](
+	[IdFactura] [int] IDENTITY(1,1) NOT NULL,
+	[IdCliente] [int] NOT NULL,
+	[IdUsuario] [varchar](50) NOT NULL,
+	[MomentoCreacion] [datetime] NOT NULL,
+ CONSTRAINT [PK_Factura] PRIMARY KEY CLUSTERED 
 (
-	@idLinkerBoxProtocolo INT
-)
-AS
-BEGIN
-    SELECT
-		*
-	FROM dbo.LinkerBoxProtocolo
-    WHERE
-		IdLinkerBoxProtocolo = @idLinkerBoxProtocolo
-END
+	[IdFactura] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 GO
+
+ALTER TABLE [facturacion].[Factura]  WITH CHECK ADD  CONSTRAINT [FK_Factura_Cliente] FOREIGN KEY([IdCliente])
+REFERENCES [administracion].[Cliente] ([IdCliente])
+GO
+
+ALTER TABLE [facturacion].[Factura] CHECK CONSTRAINT [FK_Factura_Cliente]
+GO
+
+ALTER TABLE [facturacion].[Factura]  WITH CHECK ADD  CONSTRAINT [FK_Factura_Empleado1] FOREIGN KEY([IdUsuario])
+REFERENCES [administracion].[Empleado] ([IdUsuario])
+GO
+
+ALTER TABLE [facturacion].[Factura] CHECK CONSTRAINT [FK_Factura_Empleado1]
+GO
+
+
+USE [ABC]
+GO
+
+/****** Object:  Table [logistica].[Producto]    Script Date: 01/09/2019 19:05:42 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [logistica].[Producto](
+	[IdProducto] [int] IDENTITY(1,1) NOT NULL,
+	[Nombre] [varchar](100) NOT NULL,
+	[Descripcion] [varchar](500) NULL,
+	[Precio] [real] NOT NULL,
+ CONSTRAINT [PK_Producto] PRIMARY KEY CLUSTERED 
+(
+	[IdProducto] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+
+
+
+USE [ABC]
+GO
+
+/****** Object:  Table [facturacion].[FacturaProducto]    Script Date: 01/09/2019 19:05:19 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [facturacion].[FacturaProducto](
+	[IdFactura] [int] NOT NULL,
+	[IdProducto] [int] NOT NULL,
+	[Cantidad] [int] NOT NULL
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [facturacion].[FacturaProducto]  WITH CHECK ADD  CONSTRAINT [FK_FacturaProducto_Factura] FOREIGN KEY([IdFactura])
+REFERENCES [facturacion].[Factura] ([IdFactura])
+GO
+
+ALTER TABLE [facturacion].[FacturaProducto] CHECK CONSTRAINT [FK_FacturaProducto_Factura]
+GO
+
+ALTER TABLE [facturacion].[FacturaProducto]  WITH CHECK ADD  CONSTRAINT [FK_FacturaProducto_Producto] FOREIGN KEY([IdProducto])
+REFERENCES [logistica].[Producto] ([IdProducto])
+GO
+
+ALTER TABLE [facturacion].[FacturaProducto] CHECK CONSTRAINT [FK_FacturaProducto_Producto]
+GO
+
+
